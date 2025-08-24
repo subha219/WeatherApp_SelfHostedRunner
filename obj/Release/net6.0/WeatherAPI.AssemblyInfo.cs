bebc22db6dc2531ef247a7ef954a278ee0104f88
subha_studio <sdsubha8@gmail.com>
2025-08-24 15:25:24 +0530

    changes in program cs

--- obj/Release/net6.0/WeatherAPI.AssemblyInfo.cs
+++ obj/Release/net6.0/WeatherAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944a2cc85ff32b4993a0ce995c00131773d7e644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6c728f89c4c9151fb525e24877dec9f80a2846")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
