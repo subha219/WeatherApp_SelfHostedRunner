2436bc39b6137854247f38d257eb7456ae279dc4
subha_studio <sdsubha8@gmail.com>
2025-08-24 15:38:06 +0530

    changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,16 +30,5 @@ namespace WeatherAPI.Controllers
             .ToArray();
         }
 
-        [HttpGet(Name = "GetAllWeatherForecast")]
-        public IEnumerable<WeatherForecast> GetAll()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
